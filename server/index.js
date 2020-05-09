8d68d4fa79b52d00eb10f7ef65c7b1f7199bcb55
Priyanka Miranda <priyankamiranda2@gmail.com>
2020-05-09 14:58:15 +0530

    index.js update

---
+++ server/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const cookieSession = require('cookie-session');
+
+const app = express();
+
+
+const PORT = process.env.PORT || 3000;
+
+var server = app.listen(PORT, function () {
+    console.log('Node server is running..');
+});
