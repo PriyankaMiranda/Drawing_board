3eaccf7e7881ce91a58a6953d6237a665cf9c770
Priyanka Miranda <priyankamiranda2@gmail.com>
2020-05-09 16:09:19 +0530

    Testing-5

--- server/index.js
+++ server/index.js
@@ -8,10 +8,11 @@
 
 const express = require('express');
 const cookieSession = require('cookie-session');
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
 
 const app = express();
+
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
 const port = process.env.PORT || 3000;
 
