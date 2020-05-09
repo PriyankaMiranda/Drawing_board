5d67a15269eb391d1dcd821ca1f77459f81baebc
Priyanka Miranda <priyankamiranda2@gmail.com>
2020-05-09 16:15:56 +0530

    Testing-8

--- server/index.js
+++ server/index.js
@@ -1,31 +1,8 @@
 
 
+var express = require('express');
+var app = express();
 
-
-
-
-
-
-const express = require('express');
-const cookieSession = require('cookie-session');
-
-const app = express();
-
-// const http = require('http').Server(app);
-// const io = require('socket.io')(http);
-
-const port = process.env.PORT || 3000;
-
-var server = app.listen(PORT, function () {
+var server = app.listen(5000, function () {
     console.log('Node server is running..');
 });
-
-// app.use(express.static(__dirname + '/public'));
-
-// function onConnection(socket){
-//   socket.on('drawing', (data) => socket.broadcast.emit('drawing', data));
-// }
-
-// io.on('connection', onConnection);
-
-// http.listen(port, () => console.log('listening on port ' + port));
