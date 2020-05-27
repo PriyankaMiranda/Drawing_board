adf48ed9359c9b4ccfba68b34c29aabe493e7966
Priyanka Miranda <priyankamiranda2@gmail.com>
2020-05-27 20:14:36 +0530

    Production mode homepage,lobby ready

--- server/app.js
+++ server/app.js
@@ -1,94 +1,121 @@
-const express = require('express');
-const path = require('path');
-const routes = require('./routes');
-const cookieSession = require('cookie-session');
-const keys = require('./constants');
-const port = process.env.PORT || 3000	
+const express = require("express");
+const path = require("path");
+const routes = require("./routes");
+const cookieSession = require("cookie-session");
+const keys = require("./constants");
+const port = process.env.PORT || 3000;
 
 const app = express();
 
 // Set the default views directory to html folder
-app.set('views', path.join(__dirname, 'html'));
-// Set the folder for css 
-app.use(express.static(path.join(__dirname,'css')));
+app.set("views", path.join(__dirname, "html"));
+// Set the folder for css
+app.use(express.static(path.join(__dirname, "css")));
 // Set the folder for java script
-app.use(express.static(path.join(__dirname,'js')));
+app.use(express.static(path.join(__dirname, "js")));
 // Set the folder for assets
-app.use(express.static(path.join(__dirname,'assets')));
-app.use(express.static(path.join(__dirname, 'node_modules')));
+app.use(express.static(path.join(__dirname, "assets")));
+app.use(express.static(path.join(__dirname, "node_modules")));
 
 // Set the view engine to ejs
-app.set('view engine', 'ejs');
-app.use('/', routes);
+app.set("view engine", "ejs");
+app.use("/", routes);
 
-app.use(cookieSession({maxAge: 30*24*60*60*1000,keys: [keys.cookieKey]}));
+app.use(
+	cookieSession({ maxAge: 30 * 24 * 60 * 60 * 1000, keys: [keys.cookieKey] })
+);
 
 const server = app.listen(port, () => {
-  console.log('Server is running at port '+ port);
+	console.log("Server is running at port " + port);
 });
 
-var io = require('socket.io')(server);
-var ejs = require('ejs');
-
-
-// Chatroom
+var io = require("socket.io")(server);
+var ejs = require("ejs");
 
 var chars = [];
-var imgs=[];
-
+var imgs = [];
 var disp_chars = [];
-var disp_imgs=[];
+var disp_imgs = [];
 
-var imgUsers = [];
+io.on("connection", (socket) => {
+	var addedUser = false;
 
-
-io.on('connection', (socket) => {
-  var addedUser = false;
-
-	socket.on('load chars', () => {
+	socket.on("load chars", () => {
 		chars = [];
-		imgs=[];
-		socket.broadcast.emit('get chars');
-		socket.emit('in case no one is in lobby');
+		imgs = [];
+		socket.broadcast.emit("get chars");
+		socket.emit("in case no one is in lobby");
 	});
 
-	socket.on('send chars', (data) => {
-		if (data.username == "" && data.img == ""){
+	socket.on("send chars", (data) => {
+		if (data.username == "" && data.img == "") {
 			//nothing happens
-		}
-		else if (!chars.includes(data.username) && !imgs.includes(data.img)){
+		} else if (!chars.includes(data.username) && !imgs.includes(data.img)) {
 			chars.push(data.username);
 			imgs.push(data.img);
 		}
 		socket.imgs = imgs;
-		socket.emit('hide chars globally', {imgs:socket.imgs});	
+		socket.emit("hide chars globally", { imgs: socket.imgs });
 	});
 
-	socket.on('reload chars for others on homepage', () => {
+	socket.on("reload chars for others on homepage", () => {
 		chars = [];
-		imgs=[];
-		socket.emit('get chars for reloading');
-		socket.broadcast.emit('get chars for reloading');
+		imgs = [];
+		socket.emit("get chars for reloading");
+		socket.broadcast.emit("get chars for reloading");
 	});
 
-	socket.on('send chars for homepage', (data) => {
-		if (!chars.includes(data.username) && !imgs.includes(data.img)){
+	socket.on("reload chars for others not the one that left", () => {
+		disp_chars = [];
+		disp_imgs = [];
+		socket.emit("reload chars upon disconnection");
+	});
+
+	socket.on("reload chars for others except the one that left", (data) => {
+		if (!disp_chars.includes(data.username) && !disp_imgs.includes(data.img)) {
+			disp_chars.push(data.username);
+			disp_imgs.push(data.img);
+		}
+		socket.disp_chars = disp_chars;
+		socket.disp_imgs = disp_imgs;
+
+		socket.emit("display chars lobby", {
+			chars: socket.disp_chars,
+			imgs: socket.disp_imgs,
+		});
+		socket.broadcast.emit("display chars lobby", {
+			chars: socket.disp_chars,
+			imgs: socket.disp_imgs,
+		});
+		
+	});
+
+
+
+
+	socket.on("send chars for homepage", (data) => {
+		if (!chars.includes(data.username) && !imgs.includes(data.img)) {
 			chars.push(data.username);
 			imgs.push(data.img);
 		}
 		socket.imgs = imgs;
-		socket.broadcast.emit('hide chars globally', {imgs:socket.imgs});	
+		socket.broadcast.emit("hide chars globally", { imgs: socket.imgs });
 	});
 
-	socket.on('load chars on lobby', () => {
+
+
+	socket.on("load chars on lobby", () => {
 		disp_chars = [];
-		disp_imgs=[];
-		socket.emit('get chars for lobby');
-		socket.broadcast.emit('get chars for lobby');
+		disp_imgs = [];
+		socket.emit("get chars for lobby");
+		socket.broadcast.emit("get chars for lobby");
 	});
 
-	socket.on('send chars for lobby', (data) => {
-		if (!disp_chars.includes(data.username) && !disp_imgs.includes(data.img)){
+	socket.on("send chars for lobby", (data) => {
+		if (
+			!disp_chars.includes(data.username) &&
+			!disp_imgs.includes(data.img)
+		) {
 			disp_chars.push(data.username);
 			disp_imgs.push(data.img);
 		}
@@ -96,96 +123,63 @@ io.on('connection', (socket) => {
 		socket.disp_chars = disp_chars;
 		socket.disp_imgs = disp_imgs;
 
-		socket.emit('display chars lobby', {
-			chars:socket.disp_chars,
-			imgs:socket.disp_imgs
-		});	
-		socket.broadcast.emit('display chars lobby', {
-			chars:socket.disp_chars,
-			imgs:socket.disp_imgs
-		});	
+		socket.emit("display chars lobby", {
+			chars: socket.disp_chars,
+			imgs: socket.disp_imgs,
+		});
+		socket.broadcast.emit("display chars lobby", {
+			chars: socket.disp_chars,
+			imgs: socket.disp_imgs,
+		});
 	});
 
+	// when the client emits 'new message', this listens and executes
+	socket.on("new message", (data) => {
+		socket.username = data.username;
+		socket.message = data.message;
+		// we tell the client to execute 'new message'
+		socket.broadcast.emit("new message", {
+			username: socket.username,
+			message: socket.message,
+		});
+	});
 
+	// when the client emits 'add user', this listens and executes
+	socket.on("add user", (data) => {
+		if (addedUser) return;
+		// we store the username in the socket session for this client
+		socket.username = data.username;
+		socket.message = data.message;
+		addedUser = true;
 
-  // when the client emits 'new message', this listens and executes
-  socket.on('new message', (data) => {
-    socket.username = data.username;
-    socket.message = data.message;
-    // we tell the client to execute 'new message'
-    socket.broadcast.emit('new message', {
-      username: socket.username,
-      message: socket.message
-    });
-  });
+		// echo globally (all clients) that a person has connected
+		socket.broadcast.emit("user joined", {
+			username: socket.username,
+			message: socket.message,
+		});
+	});
 
-  // when the client emits 'add user', this listens and executes
-  socket.on('add user', (data) => {
-    if (addedUser) return;
+	// when the client emits 'typing', we broadcast it to others
+	socket.on("typing", (data) => {
+		socket.broadcast.emit("typing", {
+			username: data.username,
+		});
+	});
 
-    // we store the username in the socket session for this client
-    socket.username = data.username;
-    socket.imgloc = data.imgloc;
-    socket.message = data.message;
-    
-    imgUsers.push(data)
-    addedUser = true;
+	// when the client emits 'stop typing', we broadcast it to others
+	socket.on("stop typing", () => {
+		socket.broadcast.emit("stop typing", {
+			username: socket.username,
+		});
+	});
 
-    // echo globally (all clients) that a person has connected
-    socket.broadcast.emit('user joined', {
-      username: socket.username,
-      imgloc: imgUsers,
-      message: socket.message
-    });
-  });
+	// when the user disconnects.. perform this
+	socket.on("disconnect", () => {
+		console.log("disconnecting")
+		socket.broadcast.emit("get chars for reloading upon disconnection");
 
-
-  // when the client emits 'typing', we broadcast it to others
-  socket.on('typing', (data) => {
-    socket.broadcast.emit('typing', {
-      username: data.username
-    });
-  });
-
-  // when the client emits 'stop typing', we broadcast it to others
-  socket.on('stop typing', () => {
-    socket.broadcast.emit('stop typing', {
-      username: socket.username
-    });
-  });
-
-  // when the user disconnects.. perform this
-  socket.on('disconnect', () => {
-      socket.broadcast.emit('user left', {
-        username: socket.username
-      });
-
-	
-	// socket.broadcast.emit('get chars on display lobby');
-
-  });
+		socket.broadcast.emit("user left", {
+			username: socket.username,
+		});
+	});
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- server/js/lobby_scripts.js
+++ server/js/lobby_scripts.js
@@ -1,94 +1,115 @@
 if (!document.cookie) {
-  window.location.href = '/';
+  window.location.href = "/";
 }
 
 var FADE_TIME = 150; // ms
 var TYPING_TIMER_LENGTH = 400; // ms
-var COLORS = ['#008b8b', '#006060', '#1b7742', '#002627','#3477db', '#870c25', '#d50000', '#d24d57','#aa2e00', '#d35400', '#aa6b51', '#554800',
-  '#1c2833', '#34515e', '#4b6a88', '#220b38','#522032', '#7d314c', '#483d8b', '#77448b','#8a2be2', '#a74165', '#9b59b6', '#db0a5b'];
+var COLORS = [
+  "#008b8b",
+  "#006060",
+  "#1b7742",
+  "#002627",
+  "#3477db",
+  "#870c25",
+  "#d50000",
+  "#d24d57",
+  "#aa2e00",
+  "#d35400",
+  "#aa6b51",
+  "#554800",
+  "#1c2833",
+  "#34515e",
+  "#4b6a88",
+  "#220b38",
+  "#522032",
+  "#7d314c",
+  "#483d8b",
+  "#77448b",
+  "#8a2be2",
+  "#a74165",
+  "#9b59b6",
+  "#db0a5b",
+];
 
 // Initialize variables
 var $window = $(window);
-var $messages = $('.messages'); // Messages area
-var $inputMessage = $('.inputMessage'); // Input message input box
-var $chatPage = $('.chat.page'); // The chatroom page
+var $messages = $(".messages"); // Messages area
+var $inputMessage = $(".inputMessage"); // Input message input box
+var $chatPage = $(".chat.page"); // The chatroom page
 var typing = false;
 var lastTypingTime;
 var username;
 var img;
 
 const cookie_val = document.cookie;
-username = cookie_val.split('name=')[1].split(';')[0];
-img = cookie_val.split('img=')[1].split(';')[0];
+username = cookie_val.split("name=")[1].split(";")[0];
+img = cookie_val.split("img=")[1].split(";")[0];
 
 var socket = io();
 
-socket.emit('reload chars for others on homepage');
+socket.emit("reload chars for others on homepage");
 
-socket.emit('load chars on lobby');
+socket.emit("load chars on lobby");
 
-socket.on('display chars lobby', (data)=>{
-  console.log("Data updated.")
-  removeParticipantsImg()
+socket.on("display chars lobby", (data) => {
+  removeParticipantsImg();
   var i;
-  for (i=0 ; i<data.chars.length ; i++){
-    addParticipantsImg({char:data.chars[i], img:data.imgs[i]})
+  for (i = 0; i < data.chars.length; i++) {
+    addParticipantsImg({ char: data.chars[i], img: data.imgs[i] });
   }
 });
 
 // Sets the client's username
 const setUsername = () => {
   // If the username is valid
-  username = cookie_val.split('name=')[1].split(';')[0];
-  img = cookie_val.split('img=')[1];
+  username = cookie_val.split("name=")[1].split(";")[0];
+  img = cookie_val.split("img=")[1];
   // Tell the server your username
   var message = $inputMessage.val();
   // Prevent markup from being injected into the message
   message = cleanInput(message);
-  var data = {username:username, imgloc:img, message: message};
-  socket.emit('add user', data);  
-}
+  var data = { username: username, message: message };
+  socket.emit("add user", data);
+};
 
 const addParticipantsImg = (data) => {
   var parent = document.getElementById("row_chars");
 
   var char_div = document.createElement("DIV");
   char_div.className = "characters";
-  char_div.style.maxWidth = "25vh"
-  char_div.style.maxHeight = "25vh"
-  char_div.style.flex= "25%";
-  char_div.style.padding= "40px";
-  char_div.style.opacity= 1;
-  char_div.style.transform = "scale(1)"; 
+  char_div.style.maxWidth = "25vh";
+  char_div.style.maxHeight = "25vh";
+  char_div.style.flex = "25%";
+  char_div.style.padding = "40px";
+  char_div.style.opacity = 1;
+  char_div.style.transform = "scale(1)";
   parent.appendChild(char_div);
 
-  var image = document.createElement("IMG");  
+  var image = document.createElement("IMG");
   image.className = "characters_img";
-  image.src = data.img;       
-  image.style.width  = '100%';
-  image.style.height  = '100%';
-  char_div.appendChild(image);  
+  image.src = data.img;
+  image.style.width = "100%";
+  image.style.height = "100%";
+  char_div.appendChild(image);
 
   var div_form = document.createElement("FORM");
   div_form.className = "characters_form";
-  // div_form.style.display = "block";
   div_form.style.textAlign = "center";
   div_form.style.fontStyle = "italic";
   div_form.style.fontFamily = "cursive";
   char_div.appendChild(div_form);
 
-  var div_label = document.createElement("LABEL");  
+  var div_label = document.createElement("LABEL");
   div_label.className = "characters_label";
-  div_label.style.width  = '100%';
-  div_label.innerHTML = data.char; 
+  div_label.style.width = "100%";
+  div_label.innerHTML = data.char;
   div_form.appendChild(div_label);
-
-}
+};
 
 const removeParticipantsImg = (data) => {
   var parent = document.getElementById("row_chars");
-  while ( parent.firstChild ) parent.removeChild( parent.firstChild );
-}
+  while (parent.firstChild) parent.removeChild(parent.firstChild);
+};
 
 // Sends a chat message
 const sendMessage = () => {
@@ -96,219 +117,224 @@ const sendMessage = () => {
   // Prevent markup from being injected into the message
   message = cleanInput(message);
   if (message) {
-    $inputMessage.val('');
-    var dataval = {username:username , message: message}
+    $inputMessage.val("");
+    var dataval = { username: username, message: message };
     addChatMessage(dataval);
-    socket.emit('new message', dataval);
+    socket.emit("new message", dataval);
   }
-}
+};
 
-  // Log a message
-    const log = (message, options) => {
-    var $el = $('<p>').addClass('log').text(message);
-    addMessageElement($el, options);
+// Log a message
+const log = (message, options) => {
+  var $el = $("<p>")
+    .addClass("log")
+    .text(message);
+  addMessageElement($el, options);
+};
+
+// Adds the visual chat message to the message list
+const addChatMessage = (data, options) => {
+  // Don't fade the message in if there is an 'X was typing'
+  var $typingMessages = getTypingMessages(data);
+  options = options || {};
+
+  if ($typingMessages.length !== 0) {
+    options.fade = false;
+    $typingMessages.remove();
   }
 
-  // Adds the visual chat message to the message list
-  const addChatMessage = (data, options) => {
-    // Don't fade the message in if there is an 'X was typing'
-    var $typingMessages = getTypingMessages(data);
-    options = options || {};
+  var $usernameDiv = $('<span class="username"/>')
+    .text(data.username)
+    .css("color", getUsernameColor(data.username));
+  var $messageBodyDiv = $('<span class="messageBody">').text(data.message);
 
-    if ($typingMessages.length !== 0) {
-      options.fade = false;
-      $typingMessages.remove();
+  var typingClass = data.typing ? "typing" : "";
+  var $messageDiv = $('<p class="message"/>')
+    .data("username", data.username)
+    .addClass(typingClass)
+    .append($usernameDiv, $messageBodyDiv);
+
+  addMessageElement($messageDiv, options);
+};
+
+// Adds the visual chat typing message
+const addChatTyping = (data) => {
+  data.typing = true;
+  data.message = "is typing....";
+  addChatMessage(data);
+};
+
+// Removes the visual chat typing message
+const removeChatTyping = (data) => {
+  getTypingMessages(data).fadeOut(function() {
+    $(this).remove();
+  });
+};
+
+// Adds a message element to the messages and scrolls to the bottom
+// el - The element to add as a message
+// options.fade - If the element should fade-in (default = true)
+// options.prepend - If the element should prepend
+//   all other messages (default = false)
+const addMessageElement = (el, options) => {
+  var $el = $(el);
+
+  // Setup default options
+  if (!options) {
+    options = {};
+  }
+  if (typeof options.fade === "undefined") {
+    options.fade = true;
+  }
+  if (typeof options.prepend === "undefined") {
+    options.prepend = false;
+  }
+
+  // Apply options
+  if (options.fade) {
+    $el.hide().fadeIn(FADE_TIME);
+  }
+  if (options.prepend) {
+    $messages.prepend($el);
+  } else {
+    $messages.append($el);
+  }
+  $messages[0].scrollTop = $messages[0].scrollHeight;
+};
+
+// Prevents input from having injected markup
+const cleanInput = (input) => {
+  return $("<div/>")
+    .text(input)
+    .html();
+};
+
+// Updates the typing event
+const updateTyping = () => {
+  if (!typing) {
+    typing = true;
+    username = cookie_val.split("name=")[1].split(";")[0];
+    socket.emit("typing", { username: username });
+  }
+
+  lastTypingTime = new Date().getTime();
+  setTimeout(() => {
+    var typingTimer = new Date().getTime();
+    var timeDiff = typingTimer - lastTypingTime;
+    if (timeDiff >= TYPING_TIMER_LENGTH && typing) {
+      socket.emit("stop typing");
+      typing = false;
     }
+  }, TYPING_TIMER_LENGTH);
+};
 
-    var $usernameDiv = $('<span class="username"/>')
-      .text(data.username)
-      .css('color', getUsernameColor(data.username));
-    var $messageBodyDiv = $('<span class="messageBody">')
-      .text(data.message);
+// Gets the 'X is typing' messages of a user
+const getTypingMessages = (data) => {
+  return $(".typing.message").filter(function(i) {
+    return data.username;
+  });
+};
 
-    var typingClass = data.typing ? 'typing' : '';
-    var $messageDiv = $('<p class="message"/>')
-      .data('username', data.username)
-      .addClass(typingClass)
-      .append($usernameDiv, $messageBodyDiv);
-
-    addMessageElement($messageDiv, options);
+// Gets the color of a username through our hash function
+const getUsernameColor = (username) => {
+  // Compute hash code
+  var hash = 7;
+  for (var i = 0; i < username.length; i++) {
+    hash = username.charCodeAt(i) + (hash << 5) - hash;
   }
+  // Calculate color
+  var index = Math.abs(hash % COLORS.length);
+  return COLORS[index];
+};
 
-  // Adds the visual chat typing message
-  const addChatTyping = (data) => {
-    data.typing = true;
-    data.message = 'is typing....';
-    addChatMessage(data);
-  }
+// Keyboard events
 
-  // Removes the visual chat typing message
-  const removeChatTyping = (data) => {
-    getTypingMessages(data).fadeOut(function () {
-      $(this).remove();
-    });
-  }
-
-  // Adds a message element to the messages and scrolls to the bottom
-  // el - The element to add as a message
-  // options.fade - If the element should fade-in (default = true)
-  // options.prepend - If the element should prepend
-  //   all other messages (default = false)
-  const addMessageElement = (el, options) => {
-    var $el = $(el);
-
-    // Setup default options
-    if (!options) {
-      options = {};
-    }
-    if (typeof options.fade === 'undefined') {
-      options.fade = true;
-    }
-    if (typeof options.prepend === 'undefined') {
-      options.prepend = false;
-    }
-
-    // Apply options
-    if (options.fade) {
-      $el.hide().fadeIn(FADE_TIME);
-    }
-    if (options.prepend) {
-      $messages.prepend($el);
-    } else {
-      $messages.append($el);
-    }
-    $messages[0].scrollTop = $messages[0].scrollHeight;
-  }
-
-  // Prevents input from having injected markup
-  const cleanInput = (input) => {
-    return $('<div/>').text(input).html();
-  }
-
-  // Updates the typing event
-  const updateTyping = () => {
-    
-      if (!typing) {
-        typing = true;
-        username = cookie_val.split('name=')[1].split(';')[0];
-        socket.emit('typing', {username: username});
-      }
-
-      lastTypingTime = (new Date()).getTime();
-      setTimeout(() => {
-        var typingTimer = (new Date()).getTime();
-        var timeDiff = typingTimer - lastTypingTime;
-        if (timeDiff >= TYPING_TIMER_LENGTH && typing) {
-          socket.emit('stop typing');
-          typing = false;
-        }
-      }, TYPING_TIMER_LENGTH);
-    
-  }
-
-  // Gets the 'X is typing' messages of a user
-  const getTypingMessages = (data) => {
-    return $('.typing.message').filter(function (i) {
-      return data.username;
-    });
-  }
-
-  // Gets the color of a username through our hash function
-  const getUsernameColor = (username) => {
-    // Compute hash code
-    var hash = 7;
-    for (var i = 0; i < username.length; i++) {
-       hash = username.charCodeAt(i) + (hash << 5) - hash;
-    }
-    // Calculate color
-    var index = Math.abs(hash % COLORS.length);
-    return COLORS[index];
-  }
-
-  // Keyboard events
-
-  $window.keydown(event => {
-    // When the client hits ENTER on their keyboard
-    if (event.which === 13) {
-      if (username) {
-        sendMessage();
-        socket.emit('stop typing');
-        typing = false;
-      } else {
-        setUsername();
-        sendMessage();
-      }
-    }
-  });
-
-  $inputMessage.on('input', () => {
-    updateTyping();
-  });
-
-  // Click events
-
-
-  // Focus input when clicking on the message input's border
-  $inputMessage.click(() => {
-    $inputMessage.focus();
-  });
-
-  // Socket events
-
-  //when new user on front page requests for the characters to be loaded
-  socket.on('get chars', ()=>{
-    socket.emit('send chars', {username:username,img:img});
-  });
-
-  socket.on('get chars for reloading', ()=>{
-    socket.emit('send chars for homepage', {username:username,img:img});
-  });
-
-  socket.on('get chars for lobby', ()=>{
-    socket.emit('send chars for lobby', {username:username,img:img});
-  });
-
-  // Whenever the server emits 'new message', update the chat body
-  socket.on('new message', (data) => {
-    addChatMessage(data);
-  });
-
-  // Whenever the server emits 'user joined', log it in the chat body
-  socket.on('user joined', (data) => {
-    addChatMessage(data);
-  });
-
-  // Whenever the server emits 'user left', log it in the chat body
-  socket.on('user left', (data) => {
-    // addParticipantsMessage(data);
-    removeChatTyping(data);
-  });
-
-  // Whenever the server emits 'typing', show the typing message
-  socket.on('typing', (data) => {
-    addChatTyping(data);
-  });
-
-  // Whenever the server emits 'stop typing', kill the typing message
-  socket.on('stop typing', (data) => {
-    removeChatTyping(data);
-  });
-
-  socket.on('disconnect', () => {
-    log('you have been disconnected');
-  });
-
-  socket.on('reconnect', () => {
-    log('you have been reconnected');
+$window.keydown((event) => {
+  // When the client hits ENTER on their keyboard
+  if (event.which === 13) {
     if (username) {
-      socket.emit('add user', username);
+      sendMessage();
+      socket.emit("stop typing");
+      typing = false;
+    } else {
+      setUsername();
+      sendMessage();
     }
-  });
+  }
+});
 
-  socket.on('reconnect_error', () => {
-    log('attempt to reconnect has failed');
-  });
+$inputMessage.on("input", () => {
+  updateTyping();
+});
 
+// Click events
 
+// Focus input when clicking on the message input's border
+$inputMessage.click(() => {
+  $inputMessage.focus();
+});
 
+// Socket events
 
+//when new user on front page requests for the characters to be loaded
+socket.on("get chars", () => {
+  socket.emit("send chars", { username: username, img: img });
+});
+
+socket.on("get chars for reloading", () => {
+  socket.emit("send chars for homepage", {username: username, img: img });
+});
+
+socket.on("get chars for reloading upon disconnection", () => {
+
+  socket.emit("reload chars for others not the one that left", {username: username, img: img });
+
+});
+
+socket.on("reload chars upon disconnection", () => {
+  socket.emit("reload chars for others except the one that left", {username: username, img: img });
+});
+
+socket.on("get chars for lobby", () => {
+  socket.emit("send chars for lobby", { username: username, img: img });
+});
+
+// Whenever the server emits 'new message', update the chat body
+socket.on("new message", (data) => {
+  addChatMessage(data);
+});
+
+// Whenever the server emits 'user joined', log it in the chat body
+socket.on("user joined", (data) => {
+  addChatMessage(data);
+});
+
+// Whenever the server emits 'user left', log it in the chat body
+socket.on("user left", (data) => {
+  removeChatTyping(data);
+});
+
+// Whenever the server emits 'typing', show the typing message
+socket.on("typing", (data) => {
+  addChatTyping(data);
+});
+
+// Whenever the server emits 'stop typing', kill the typing message
+socket.on("stop typing", (data) => {
+  removeChatTyping(data);
+});
+
+socket.on("disconnect", () => {
+  log("you have been disconnected");
+});
+
+socket.on("reconnect", () => {
+  log("you have been reconnected");
+  if (username) {
+    socket.emit("add user", username);
+  }
+});
+
+socket.on("reconnect_error", () => {
+  log("attempt to reconnect has failed");
+});
