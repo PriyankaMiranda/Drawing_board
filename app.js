9c2648ca14bded0f5837b95e3651687dc9d2a4e3
Priyanka Miranda <priyankamiranda2@gmail.com>
2020-06-30 23:44:01 +0530

    heroku error fixing-8

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const routes = require("./routes");
 const cookieSession = require("cookie-session");
-const keys = require("keys");
+const keys = require("./keys");
 const port = process.env.PORT || 5000;
 const app = express();
 
