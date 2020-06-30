4d59ee77c7181294f47f614a1876edabb9e3c98b
Priyanka Miranda <priyankamiranda2@gmail.com>
2020-06-30 23:42:32 +0530

    heroku error fixing-7

---
+++ keys.js
@@ -0,0 +1,86 @@
+module.exports = {
+	cookieKey: 'asdfnnovoe!27t43',
+	herokuSite:'https://hidden-ravine-98503.herokuapp.com/ ',
+	mongodb:'mongodb+srv://myawesomecluster:test1awesome29@mycluster-vm2t8.mongodb.net/test?retryWrites=true&w=majority',	
+	
+	question1:"Pour coke into my dead body's mouth, okay?" ,
+	answer1:'ron',
+
+	question2:'To think my boobs brought you guys closer',
+	answer2:'sam',
+
+	question3:'Dating the constitution would be weird',
+	answer3:'derick',
+
+	question4:'Just need to cover up the evidence',
+	answer4:'susan',
+
+	question5:"Told you, don't trust coke bottles.",
+	answer5:'sheikh',
+
+	question6:"I'd like to thinkn those are squealy farts",
+	answer6:'derick',
+
+	question7:'Grow some boobs and you can be a part of this rob',
+	answer7:'susan',
+
+	question8:'Is the graph paper your body. I would love to write 100 words all over it',
+	answer8:'tina',
+
+	question9:'I asked boobs. Who likes ass? Chee',
+	answer9:'ron',
+
+	question10:"I decline for now. The day I whip myself into shape, you'll get some",
+	answer10:'derick',
+
+	question12:"I'm already your slave, Susan",
+	answer12:'ron',
+
+	question13:"Lets all congregate after the lockdown ends",
+	answer13:'tejas',
+
+	question14:"I didn't come for a mere threesome. Get a 4th person!",
+	answer14:'hannah',
+
+	question15:'Pfft, what peasant water do you drink?',
+	answer15:'tejas',
+
+	question16:"I'm not leaving earth without hitting a 100",
+	answer16:'toby',
+
+	question17:"I'm tamilian, so the baby should die",
+	answer17:'tejas',
+
+	question18:"Everybody gangsta till I whilp out thanos's dick",
+	answer18:'gautham',
+
+	question19:'Cheating. The devil lives in the TV',
+	answer19:'ashish',
+
+	question20:'I prefer circumcised',
+	answer20:'hannah',
+
+	question21:"Titty for tatti" ,
+	answer21:'vishnu',
+
+	question22:"You're alive and welcome",
+	answer22:'venisha',
+
+	question23:'I pee on you',
+	answer23:'venisha',
+
+	question24:'Laughing on the outside, strong on the inside',
+	answer24:'jenny',
+
+	question25:"Don't gimme your anxiety",
+	answer25:'sandra',
+
+	question26:"You had to pay for fire?",
+	answer26:'venisha',
+
+	question27:"Don't worry, I slept next to a cat",
+	answer27:'susan'
+
+}
+
+
