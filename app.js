d767498a9c185b80aba0d60b2f95abe2be03b0cb
Priyanka Miranda <priyankamiranda2@gmail.com>
2020-06-30 23:27:57 +0530

    heroku error fixing-4

--- app.js
+++ app.js
@@ -32,192 +32,192 @@ const server = app.listen(port, () => {
 var io = require("socket.io")(server);
 var ejs = require("ejs");
 
-var chars = [];//for homepage
-var imgs = [];//for homepage
-var disp_chars = [];//for lobby
-var disp_imgs = [];//for lobby
+// var chars = [];//for homepage
+// var imgs = [];//for homepage
+// var disp_chars = [];//for lobby
+// var disp_imgs = [];//for lobby
 
-io.on("connection", (socket) => {
-	var addedUser = false;
+// io.on("connection", (socket) => {
+// 	var addedUser = false;
 
-	socket.on("new user", () => {
-		socket.emit("private key",{cookieKey:keys.cookieKey});
-		socket.emit("new user");
-	});
+// 	socket.on("new user", () => {
+// 		socket.emit("private key",{cookieKey:keys.cookieKey});
+// 		socket.emit("new user");
+// 	});
 
-	socket.on("check ans", (data) => {
-		socket.emit("answer",{ans:ans[data.x]});
-	});
+// 	socket.on("check ans", (data) => {
+// 		socket.emit("answer",{ans:ans[data.x]});
+// 	});
 
-	socket.on("authenticate", () => {
-		socket.emit("authenticate",{cookieKey:keys.cookieKey});
-	});
+// 	socket.on("authenticate", () => {
+// 		socket.emit("authenticate",{cookieKey:keys.cookieKey});
+// 	});
 
-	socket.on("load chars", () => {
-		chars = [];
-		imgs = [];
-		socket.broadcast.emit("get chars");
-		socket.emit("in case no one is in lobby");
-	});
+// 	socket.on("load chars", () => {
+// 		chars = [];
+// 		imgs = [];
+// 		socket.broadcast.emit("get chars");
+// 		socket.emit("in case no one is in lobby");
+// 	});
 
-	socket.on("send chars", (data) => {
-		if (data.username == "" && data.img == "") {
-			// nothing happens
-		} else if (!chars.includes(data.username) && !imgs.includes(data.img)) {
-			chars.push(data.username);
-			imgs.push(data.img);
-		}
-		socket.imgs = imgs;
-		socket.emit("hide chars globally", { imgs: socket.imgs });
-	});
+// 	socket.on("send chars", (data) => {
+// 		if (data.username == "" && data.img == "") {
+// 			// nothing happens
+// 		} else if (!chars.includes(data.username) && !imgs.includes(data.img)) {
+// 			chars.push(data.username);
+// 			imgs.push(data.img);
+// 		}
+// 		socket.imgs = imgs;
+// 		socket.emit("hide chars globally", { imgs: socket.imgs });
+// 	});
 
-	socket.on("reload chars for others on homepage", () => {
-		chars = [];
-		imgs = [];
-		socket.emit("get chars for reloading");
-		socket.broadcast.emit("get chars for reloading");
-	});
-
-
-	socket.on("reload chars for others not the one that left", () => {
-		disp_chars = [];
-		disp_imgs = [];
-		socket.emit("reload chars upon disconnection");
-	});
-
-	socket.on("reload chars for others except the one that left", (data) => {
-		if (!disp_chars.includes(data.username) && !disp_imgs.includes(data.img)) {
-			disp_chars.push(data.username);
-			disp_imgs.push(data.img);
-		}
-
-		socket.disp_chars = disp_chars;
-		socket.disp_imgs = disp_imgs;
-
-		socket.emit("display chars lobby", {
-			chars: socket.disp_chars,
-			imgs: socket.disp_imgs,
-		});
-		socket.broadcast.emit("display chars lobby", {
-			chars: socket.disp_chars,
-			imgs: socket.disp_imgs,
-		});
-	});
+// 	socket.on("reload chars for others on homepage", () => {
+// 		chars = [];
+// 		imgs = [];
+// 		socket.emit("get chars for reloading");
+// 		socket.broadcast.emit("get chars for reloading");
+// 	});
 
 
+// 	socket.on("reload chars for others not the one that left", () => {
+// 		disp_chars = [];
+// 		disp_imgs = [];
+// 		socket.emit("reload chars upon disconnection");
+// 	});
 
-	socket.on("send chars for homepage", (data) => {
-		if (!chars.includes(data.username) && !imgs.includes(data.img)) {
-			chars.push(data.username);
-			imgs.push(data.img);
-		}
-		socket.imgs = imgs;
-		socket.broadcast.emit("hide chars globally", { imgs: socket.imgs });
-	});
+// 	socket.on("reload chars for others except the one that left", (data) => {
+// 		if (!disp_chars.includes(data.username) && !disp_imgs.includes(data.img)) {
+// 			disp_chars.push(data.username);
+// 			disp_imgs.push(data.img);
+// 		}
+
+// 		socket.disp_chars = disp_chars;
+// 		socket.disp_imgs = disp_imgs;
+
+// 		socket.emit("display chars lobby", {
+// 			chars: socket.disp_chars,
+// 			imgs: socket.disp_imgs,
+// 		});
+// 		socket.broadcast.emit("display chars lobby", {
+// 			chars: socket.disp_chars,
+// 			imgs: socket.disp_imgs,
+// 		});
+// 	});
 
 
 
-	socket.on("load chars on lobby", () => {		
-		disp_chars = [];
-		disp_imgs = [];
-		socket.emit("get chars for lobby");
-		socket.broadcast.emit("get chars for lobby");
-	});
+// 	socket.on("send chars for homepage", (data) => {
+// 		if (!chars.includes(data.username) && !imgs.includes(data.img)) {
+// 			chars.push(data.username);
+// 			imgs.push(data.img);
+// 		}
+// 		socket.imgs = imgs;
+// 		socket.broadcast.emit("hide chars globally", { imgs: socket.imgs });
+// 	});
 
-	socket.on("refresh interval function", () => {	
-		var myInt = setInterval(function () {
-		socket.emit("get chars for lobby");
-		socket.broadcast.emit("get chars for lobby");
-		}, 500);
 
-	});	
 
-	socket.on("send chars for lobby", (data) => {
-		if (
-			!disp_chars.includes(data.username) &&
-			!disp_imgs.includes(data.img)
-		) {
-			disp_chars.push(data.username);
-			disp_imgs.push(data.img);
-		}
-		socket.broadcast.emit("delete ready button");
-		if(disp_chars.length==1 && disp_imgs.length==1){
-			socket.emit("set ready button");
-			socket.emit("refresh interval function");
-		}
+// 	socket.on("load chars on lobby", () => {		
+// 		disp_chars = [];
+// 		disp_imgs = [];
+// 		socket.emit("get chars for lobby");
+// 		socket.broadcast.emit("get chars for lobby");
+// 	});
 
-		socket.disp_chars = disp_chars;
-		socket.disp_imgs = disp_imgs;
+// 	socket.on("refresh interval function", () => {	
+// 		var myInt = setInterval(function () {
+// 		socket.emit("get chars for lobby");
+// 		socket.broadcast.emit("get chars for lobby");
+// 		}, 500);
 
-		socket.emit("display chars lobby", {
-			chars: socket.disp_chars,
-			imgs: socket.disp_imgs,
-		});
-		socket.broadcast.emit("display chars lobby", {
-			chars: socket.disp_chars,
-			imgs: socket.disp_imgs,
-		});
-	});
+// 	});	
 
-	socket.on("enter game", (data) => {
-		//remove all ready buttons
-		socket.broadcast.emit("delete ready button");
-		console.log("connected :"+socket.id);
-		socket.emit("enter game", {gameID:data.gameID});
-		socket.broadcast.emit("enter game", {gameID:data.gameID});
-	});
+// 	socket.on("send chars for lobby", (data) => {
+// 		if (
+// 			!disp_chars.includes(data.username) &&
+// 			!disp_imgs.includes(data.img)
+// 		) {
+// 			disp_chars.push(data.username);
+// 			disp_imgs.push(data.img);
+// 		}
+// 		socket.broadcast.emit("delete ready button");
+// 		if(disp_chars.length==1 && disp_imgs.length==1){
+// 			socket.emit("set ready button");
+// 			socket.emit("refresh interval function");
+// 		}
 
-	// when the client emits 'new message', this listens and executes
-	socket.on("new message", (data) => {
-		socket.username = data.username;
-		socket.message = data.message;
-		// we tell the client to execute 'new message'
-		socket.broadcast.emit("new message", {
-			username: socket.username,
-			message: socket.message,
-		});
-	});
+// 		socket.disp_chars = disp_chars;
+// 		socket.disp_imgs = disp_imgs;
 
-	// when the client emits 'add user', this listens and executes
-	socket.on("add user", (data) => {
-		if (addedUser) return;
-		// we store the username in the socket session for this client
-		socket.username = data.username;
-		socket.message = data.message;
-		addedUser = true;
+// 		socket.emit("display chars lobby", {
+// 			chars: socket.disp_chars,
+// 			imgs: socket.disp_imgs,
+// 		});
+// 		socket.broadcast.emit("display chars lobby", {
+// 			chars: socket.disp_chars,
+// 			imgs: socket.disp_imgs,
+// 		});
+// 	});
 
-		// echo globally (all clients) that a person has connected
-		socket.broadcast.emit("user joined", {
-			username: socket.username,
-			message: socket.message,
-		});
-	});
+// 	socket.on("enter game", (data) => {
+// 		//remove all ready buttons
+// 		socket.broadcast.emit("delete ready button");
+// 		console.log("connected :"+socket.id);
+// 		socket.emit("enter game", {gameID:data.gameID});
+// 		socket.broadcast.emit("enter game", {gameID:data.gameID});
+// 	});
 
-	// when the client emits 'typing', we broadcast it to others
-	socket.on("typing", (data) => {
-		socket.broadcast.emit("typing", {
-			username: data.username,
-		});
-	});
+// 	// when the client emits 'new message', this listens and executes
+// 	socket.on("new message", (data) => {
+// 		socket.username = data.username;
+// 		socket.message = data.message;
+// 		// we tell the client to execute 'new message'
+// 		socket.broadcast.emit("new message", {
+// 			username: socket.username,
+// 			message: socket.message,
+// 		});
+// 	});
 
-	// when the client emits 'stop typing', we broadcast it to others
-	socket.on("stop typing", () => {
-		socket.broadcast.emit("stop typing", {
-			username: socket.username,
-		});
-	});
+// 	// when the client emits 'add user', this listens and executes
+// 	socket.on("add user", (data) => {
+// 		if (addedUser) return;
+// 		// we store the username in the socket session for this client
+// 		socket.username = data.username;
+// 		socket.message = data.message;
+// 		addedUser = true;
 
-	// when the user disconnects.. perform this
-	socket.on("disconnect", () => {
-		socket.broadcast.emit("get chars for reloading upon disconnection");
+// 		// echo globally (all clients) that a person has connected
+// 		socket.broadcast.emit("user joined", {
+// 			username: socket.username,
+// 			message: socket.message,
+// 		});
+// 	});
 
-		socket.broadcast.emit("user left", {
-			username: socket.username,
-		});
+// 	// when the client emits 'typing', we broadcast it to others
+// 	socket.on("typing", (data) => {
+// 		socket.broadcast.emit("typing", {
+// 			username: data.username,
+// 		});
+// 	});
+
+// 	// when the client emits 'stop typing', we broadcast it to others
+// 	socket.on("stop typing", () => {
+// 		socket.broadcast.emit("stop typing", {
+// 			username: socket.username,
+// 		});
+// 	});
+
+// 	// when the user disconnects.. perform this
+// 	socket.on("disconnect", () => {
+// 		socket.broadcast.emit("get chars for reloading upon disconnection");
+
+// 		socket.broadcast.emit("user left", {
+// 			username: socket.username,
+// 		});
 		
-	});
+// 	});
 
-	socket.on('drawing', (data) => socket.broadcast.emit('drawing', data));
+// 	socket.on('drawing', (data) => socket.broadcast.emit('drawing', data));
 
 
-});
+// });
